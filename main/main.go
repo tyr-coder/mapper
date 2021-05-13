132c851484c15c32a13104a81269676f398e3ab3
tyr-coder <terrysb@gmail.com>
2021-05-13 13:07:46 -0400

    First workgin version

--- main/main.go
+++ main/main.go
@@ -2,56 +2,61 @@ package main
 
 import (
 	//"crypto/sha256"
+	"crypto/elliptic"
 	"fmt"
-	"strings"
-	"bitcoin/swu"
 	"math/big"
+	"strings"
+
+	"github.com/tyr-coder/mapper/swu"
 )
+
+var c = elliptic.P256()
+
 func main() {
 	fmt.Println("SHA256 MAPPER V0.1")
 	//sum := sha256.Sum256([]byte("a"))
 	//tryPoint([]byte("a"))
+
 	//fmt.Printf("%x", sum)
-	execSeries (2)
+	execSeries(100)
 
 }
 
 func tryPoint(r []byte) {
-	var a, b, h  = swu.HashToPoint(r)
+	var a, b, h = swu.HashToPoint(r)
 	fmt.Println("Hash Value")
-	str1 := fmt.Sprintf("0x%x",h)
-        fmt.Println("%x",str1)	
-	printPoint(a,b)
+	str1 := fmt.Sprintf("0x%x", h)
+	fmt.Println("%x", str1)
+	printPoint(a, b)
 	return
 }
 
-func printPoint(x,y *big.Int) {
-	str1 := fmt.Sprintf("0x%x",x)
-	str2 := fmt.Sprintf("0x%x",y)
+func printPoint(x, y *big.Int) {
+	str1 := fmt.Sprintf("0x%x", x)
+	str2 := fmt.Sprintf("0x%x", y)
 	fmt.Println("x= ")
-	fmt.Println("%x",str1)
+	fmt.Println("%x", str1)
 	fmt.Println("y= ")
-	fmt.Println("%x",str2)g
-}	
-
-func execSeries (maxtimes int){
-  var buf strings.Builder
-  var str = "nsa" 
-
-  for i := 1; i < maxtimes; i++ {
-
-	  arg :=  "nsa"
-	  buf.WriteString(str)
-	  buf.Reset()
-	  //buf.WriteString(arg)
-	  fmt.Println("%s",arg)
-	  arg1 := []byte(arg)
-	  a,b,h := swu.HashToPoint(arg1)
-	  fmt.Println("Hash ="); 
-          str1 := fmt.Sprintf("0x%x",h)
-	  fmt.Println("%x",str1)
-	  printPoint(a,b)
-  }
-
+	fmt.Println("%x", str2)
+	if c.IsOnCurve(x, y) {
+		fmt.Println("on curve")
+	}
 }
 
+func execSeries(maxtimes int) {
+	var buf strings.Builder
+	var str = "nsa"
+
+	for i := 1; i < maxtimes; i++ {
+		buf.WriteString(str)
+		var arg1 = []byte(buf.String())
+		a, b, h := swu.HashToPoint(arg1)
+		fmt.Println("Input:")
+		fmt.Println(buf.String())
+		fmt.Println("Hash =")
+		str1 := fmt.Sprintf("0x%x", h)
+		fmt.Println("%x", str1)
+		printPoint(a, b)
+	}
+
+}
